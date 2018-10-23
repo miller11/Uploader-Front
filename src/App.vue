4dfad933ff272178cbd3c19db4a401d1498f1ec8
rhmiller <ross-h-miller@uiowa.edu>
2018-10-23 16:37:10 -0500

    adding files component

--- src/App.vue
+++ src/App.vue
@@ -28,155 +28,157 @@
       </div>
     </nav>
 
-    <header>
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-          <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Big%20Stopper.jpg')">
-            <div class="carousel-caption d-none d-md-block">
-              <!--<h3>First Slide</h3>-->
-              <p>Waimea bay</p>
-            </div>
-          </div>
-          <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Colton-Butterfly.jpg')">
-            <div class="carousel-caption d-none d-md-block">
-              <!--<h3>Second Slide</h3>-->
-              <p>Colton swimming butterfly</p>
-            </div>
-          </div>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Milky%20Way-XL.jpg')">
-            <div class="carousel-caption d-none d-md-block">
-              <!--<h3>Third Slide</h3>-->
-              <p>Milky way picture taken in Hawaii</p>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </header>
+    <!--<header>-->
+      <!--<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">-->
+        <!--<ol class="carousel-indicators">-->
+          <!--<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
+          <!--<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
+          <!--<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
+        <!--</ol>-->
+        <!--<div class="carousel-inner" role="listbox">-->
+          <!--&lt;!&ndash; Slide One - Set the background image for this slide in the line below &ndash;&gt;-->
+          <!--<div class="carousel-item active" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Big%20Stopper.jpg')">-->
+            <!--<div class="carousel-caption d-none d-md-block">-->
+              <!--&lt;!&ndash;<h3>First Slide</h3>&ndash;&gt;-->
+              <!--<p>Waimea bay</p>-->
+            <!--</div>-->
+          <!--</div>-->
+          <!--&lt;!&ndash; Slide Two - Set the background image for this slide in the line below &ndash;&gt;-->
+          <!--<div class="carousel-item" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Colton-Butterfly.jpg')">-->
+            <!--<div class="carousel-caption d-none d-md-block">-->
+              <!--&lt;!&ndash;<h3>Second Slide</h3>&ndash;&gt;-->
+              <!--<p>Colton swimming butterfly</p>-->
+            <!--</div>-->
+          <!--</div>-->
+          <!--&lt;!&ndash; Slide Three - Set the background image for this slide in the line below &ndash;&gt;-->
+          <!--<div class="carousel-item" style="background-image: url('https://storage.googleapis.com/www.ic-miller.net/images/Milky%20Way-XL.jpg')">-->
+            <!--<div class="carousel-caption d-none d-md-block">-->
+              <!--&lt;!&ndash;<h3>Third Slide</h3>&ndash;&gt;-->
+              <!--<p>Milky way picture taken in Hawaii</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">-->
+          <!--<span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+          <!--<span class="sr-only">Previous</span>-->
+        <!--</a>-->
+        <!--<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">-->
+          <!--<span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+          <!--<span class="sr-only">Next</span>-->
+        <!--</a>-->
+      <!--</div>-->
+    <!--</header>-->
+
+    <u-files></u-files>
 
     <!-- Page Content -->
-    <div class="container">
+    <!--<div class="container">-->
 
-      <h1 class="my-4">Welcome to Bob's Photos</h1>
+      <!--<h1 class="my-4">Welcome to Bob's Photos</h1>-->
 
-      <div class="row">
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Choir Night</a>
-              </h4>
-              <p class="card-text">Photos from choir night. </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Christmas 2018</a>
-              </h4>
-              <p class="card-text">Photos taken at Christmas 2018.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">New York 2018</a>
-              </h4>
-              <p class="card-text">Pictures taken on New York Trip 2018.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Senior Photos</a>
-              </h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Iowa Football</a>
-              </h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="#">Hawaii</a>
-              </h4>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /.row -->
+      <!--<div class="row">-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">Choir Night</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Photos from choir night. </p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">Christmas 2018</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Photos taken at Christmas 2018.</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">New York 2018</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Pictures taken on New York Trip 2018.</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">Senior Photos</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">Iowa Football</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="col-lg-4 col-sm-6 portfolio-item">-->
+          <!--<div class="card h-100">-->
+            <!--<a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>-->
+            <!--<div class="card-body">-->
+              <!--<h4 class="card-title">-->
+                <!--<a href="#">Hawaii</a>-->
+              <!--</h4>-->
+              <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--&lt;!&ndash; /.row &ndash;&gt;-->
 
-      <!-- Features Section -->
-      <div class="row">
-        <div class="col-lg-6">
-          <h2>About Bob</h2>
-          <p>Bob shoots with:</p>
-          <ul>
-            <li>Nikon</li>
-            <li>Something</li>
-            <li>Something</li>
-            <li>Working contact form with validation</li>
-            <li>Unstyled page elements for easy customization</li>
-          </ul>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>
-        </div>
-        <div class="col-lg-6">
-          <img class="img-fluid rounded" src="http://placehold.it/700x450" alt="Family Picture">
-        </div>
-      </div>
-      <!-- /.row -->
+      <!--&lt;!&ndash; Features Section &ndash;&gt;-->
+      <!--<div class="row">-->
+        <!--<div class="col-lg-6">-->
+          <!--<h2>About Bob</h2>-->
+          <!--<p>Bob shoots with:</p>-->
+          <!--<ul>-->
+            <!--<li>Nikon</li>-->
+            <!--<li>Something</li>-->
+            <!--<li>Something</li>-->
+            <!--<li>Working contact form with validation</li>-->
+            <!--<li>Unstyled page elements for easy customization</li>-->
+          <!--</ul>-->
+          <!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, omnis doloremque non cum id reprehenderit, quisquam totam aspernatur tempora minima unde aliquid ea culpa sunt. Reiciendis quia dolorum ducimus unde.</p>-->
+        <!--</div>-->
+        <!--<div class="col-lg-6">-->
+          <!--<img class="img-fluid rounded" src="http://placehold.it/700x450" alt="Family Picture">-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--&lt;!&ndash; /.row &ndash;&gt;-->
 
-      <hr>
+      <!--<hr>-->
 
-      <!-- Call to Action Section -->
-      <div class="row mb-4">
-        <div class="col-md-8">
-          <p>These photos are property of the photographer and this website and may only be used with permission.</p>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-lg btn-secondary btn-block" href="#">Contact</a>
-        </div>
-      </div>
+      <!--&lt;!&ndash; Call to Action Section &ndash;&gt;-->
+      <!--<div class="row mb-4">-->
+        <!--<div class="col-md-8">-->
+          <!--<p>These photos are property of the photographer and this website and may only be used with permission.</p>-->
+        <!--</div>-->
+        <!--<div class="col-md-4">-->
+          <!--<a class="btn btn-lg btn-secondary btn-block" href="#">Contact</a>-->
+        <!--</div>-->
+      <!--</div>-->
 
-    </div>
+    <!--</div>-->
     <!-- /.container -->
 
     <!-- Footer -->
@@ -201,6 +203,8 @@
   import {provider} from "./firebaseConfig";
   import {store} from './store/store';
 
+  import Files from './Files'
+
   Firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       store.dispatch('setUser', user);
@@ -215,6 +219,9 @@
       return {
         msg: 'Welcome to Your Vue.js App'
       }
+    },
+    components :{
+      uFiles: Files
     },
     methods: {
       login() {
