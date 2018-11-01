bd013a673b6f4171b8d3dd478f01f8ab69a46be4
miller11 <ross-h-miller@uiowa.edu>
2018-10-31 21:14:31 -0500

    working on routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import Home from './components/Home'
 import ManageAlbums from './components/ManageAlbums'
-import Album from './components/Album'
+import AlbumManage from './components/AlbumManage'
 
 export const routes = [
   {path: '/', name: 'homeLink', component: Home},
-  {path: '/albums', name: 'albumsLink', component: ManageAlbums},
-  {path: '/albums/:albumKey', name: 'albumLink', component: Album},
+  {path: '/albums/manage', name: 'albumsLink', component: ManageAlbums},
+  {path: '/albums/manage/:albumKey', name: 'albumLink', component: AlbumManage},
   {path: '*', redirect: '/' }
 ];
