56f78afc9f1ffd62b71c90a8d10618d39d454c28
rhmiller <ross-h-miller@uiowa.edu>
2018-11-02 08:38:38 -0500

    working on delete methods

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="col-lg-4 col-sm-6 portfolio-item">
     <div class="card h-100">
-      <a @click="showModal"><img :src="album.coverPhoto.url" :alt="album.coverPhoto.name" class="card-img-top cover-photo"></a>
+      <a @click="showModal"><img :src="album.coverPhoto.url" :alt="album.coverPhoto.name"
+                                 class="card-img-top cover-photo"></a>
       <div class="card-body">
         <h4 class="card-title">
           <a href="" class="btn-link" @click.prevent="showModal">{{ album.name }}</a>
@@ -11,11 +12,15 @@
 
       <div class="card-footer" v-if="isPhotoOwner && isEditScreen">
         <div class="float-right p-1">
-          <a class="text-danger" href="" @click.prevent="deleteAlbum"><font-awesome-icon icon="trash"></font-awesome-icon></a>
+          <a class="text-danger" href="" @click.prevent="deleteAlbum">
+            <font-awesome-icon icon="trash"></font-awesome-icon>
+          </a>
         </div>
 
         <div class="float-right p-1">
-          <router-link :to="{name: 'albumLink', params: { albumKey: albumKey }}"><font-awesome-icon icon="wrench"></font-awesome-icon></router-link>
+          <router-link :to="{name: 'albumLink', params: { albumKey: albumKey }}">
+            <font-awesome-icon icon="wrench"></font-awesome-icon>
+          </router-link>
         </div>
       </div>
     </div>
@@ -67,9 +72,9 @@
         let self = this;
 
         dbAlbumPhotosRef(this.albumKey).once('value').then(function (snapshot) {
-            if(snapshot.hasChildren()) {
-              self.photos = snapshot.val();
-            }
+          if (snapshot.hasChildren()) {
+            self.photos = snapshot.val();
+          }
         });
 
         this.modalShow = true;
@@ -77,53 +82,52 @@
       deleteAlbum() {
         let self = this;
 
-        if(confirm("Are you sure you wish to delete this album?")) {
+        if (confirm("Are you sure you wish to delete this album?")) {
           dbAlbumPhotosRef(this.albumKey).once('value').then(function (snapshot) {
-            if(snapshot.hasChildren()) {
+            if (snapshot.hasChildren()) {
               self.photos = snapshot.val();
+              self.deleteStorage();
             }
           });
+        }
 
-          for(let key in self.photos) {
+        },
+      deleteStorage() {
+        let self = this;
 
-            // delete photos in album storage
-            stAlbumPhotosRef(self.albumKey).child(self.photos[key].name).delete().then(function () {
-              // Files deleted successfully
-              console.log(self.photos[key].name + " has been deleted");
+        for (let key in self.photos) {
 
-            }).catch(function (error) {
-              console.log("An error occurred trying to delete photos: " + error)
-            });
+          console.log(self.photos[key].name);
+
+          // delete photos in album storage
+          stAlbumPhotosRef(self.albumKey).child(self.photos[key].name).delete();
+        }
+
+        // delete photos database
+        dbAlbumPhotosRef(self.albumKey).remove().then(function () {
+          // Album deleted successfully
+          console.log("Photos have been deleted");
+
+        }).catch(function (error) {
+          console.log("An error occurred trying to delete photos: " + error)
+        });
+
+        this.$emit('delete', this.albumKey);
+      }
+      },
+      computed: {
+        isPhotoOwner() {
+          return this.$store.getters.photoOwner
+        },
+        isEditScreen() {
+          if (this.editScreen != null) {
+            return this.editScreen;
           }
 
-
-
-          // delete photos database
-          dbAlbumPhotosRef(self.albumKey).remove().then(function () {
-            // Album deleted successfully
-            console.log("Album has been deleted");
-
-          }).catch(function (error) {
-            console.log("An error occurred trying to delete photos: " + error)
-          });
-
-          this.$emit('delete', this.albumKey);
+          return false;
         }
-      }
-    },
-    computed: {
-      isPhotoOwner() {
-        return this.$store.getters.photoOwner
-      },
-      isEditScreen() {
-        if(this.editScreen != null ) {
-          return this.editScreen;
-        }
-
-        return false;
       }
     }
-  }
 </script>
 
 <style scoped>
